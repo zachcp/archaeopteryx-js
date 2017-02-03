efcc23684a4b5ba3630cdc832c31da98038736b3
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-02-03 11:48:07 -0500

    basic initial working solution

---
+++ R/archaeopteryx.R
@@ -0,0 +1,66 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+archaeopteryx <- function(message, treestring, width = NULL, height = NULL, elementId = NULL) {
+
+  # forward options using x
+  x = list(
+    message = message,
+    treestring=treestring
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'archaeopteryx',
+    x,
+    width = width,
+    height = height,
+    package = 'archaeopteryx',
+    elementId = elementId
+  )
+}
+
+#' Shiny bindings for archaeopteryx
+#'
+#' Output and render functions for using archaeopteryx within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a archaeopteryx
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name archaeopteryx-shiny
+#'
+#' @export
+archaeopteryxOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'archaeopteryx', width, height, package = 'archaeopteryx')
+}
+
+#' @rdname archaeopteryx-shiny
+#' @export
+renderArchaeopteryx <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, archaeopteryxOutput, env, quoted = TRUE)
+}
+
+
+
+archaeopteryx_html <- function(id, style, class, width, height, ...) {
+  list(tags$div(id = id, class = class, style = style),
+       tags$div(id = "phylogram"),
+       tags$div(id = "controls0", class='ui-widget-content'))
+       # tags$div(id   = sprintf("phylogram", id),
+       #          style = sprintf("width:%s", width),
+       #          class = sprintf("%s-widget-content ui-widget-content", class),
+       #          HTML(sprintf("<center><label style='padding-right:5px' for='%s-select'></label><select id='%s-select' style='visibility:visible;'></select></center>", id, id))))
+}
+
---
+++ inst/htmlwidgets/archaeopteryx.js
@@ -0,0 +1,102 @@
+var tre;
+
+HTMLWidgets.widget({
+
+  name: 'archaeopteryx',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+        // TODO: code to render the widget, e.g.
+        el.innerText = x.message;
+
+        var options = {};
+        options.alignPhylogram = true;
+        options.backgroundColorDefault = '#ffffff';
+        options.branchColorDefault = '#909090';
+        options.branchDataFontSize = 6;
+        options.branchWidthDefault = 1;
+        options.collapasedLabelLength = 7;
+        options.defaultFont = 'Arial';
+        options.dynahide = true;
+        options.externalNodeFontSize = 8;
+        options.found0and1ColorDefault = '#0000ff';
+        options.found0ColorDefault = '#00ff00';
+        options.found1ColorDefault = '#ff0000';
+        options.internalNodeFontSize = 6;
+        options.labelColorDefault = '#202020';
+        options.minBranchLengthValueToShow = 0.01;
+        options.minConfidenceValueToShow = 0.5;
+        options.nameForNhDownload = 'archaeopteryx-js.nh';
+        options.nameForPhyloXmlDownload = 'archaeopteryx-js.xml';
+        options.nameForPngDownload = 'archaeopteryx-js.png';
+        options.nameForSvgDownload = 'archaeopteryx-js.svg';
+        options.nodeSizeDefault = 3;
+        options.phylogram = false;
+        options.searchIsCaseSensitive = false;
+        options.searchIsPartial = true;
+        options.searchUsesRegex = false;
+        options.showBranchEvents = true;
+        options.showBranchLengthValues = false;
+        options.showConfidenceValues = true;
+        options.showDisributions = false;
+        options.showExternalLabels = true;
+        options.showExternalNodes = false;
+        options.showInternalLabels = true;
+        options.showInternalNodes = false;
+        options.showNodeEvents = true;
+        options.showNodeName = false;
+        options.showSequence = false;
+        options.showSequenceAccession = true;
+        options.showSequenceGeneSymbol = true;
+        options.showSequenceName = true;
+        options.showSequenceSymbol = true;
+        options.showTaxonomy = true;
+        options.showTaxonomyCode = false;
+        options.showTaxonomyCommonName = true;
+        options.showTaxonomyRank = false;
+        options.showTaxonomyScientificName = true;
+        options.showTaxonomySynonyms = false;
+
+        var settings = {};
+        settings.border = '1px solid #909090';
+        settings.controls0Top = 10;
+        settings.controlsBackgroundColor = '#e0e0e0';
+        settings.controlsFont = 'Arial';
+        settings.controlsFontColor = '#505050';
+        settings.controlsFontSize = 8;
+        settings.displayHeight = height;
+        settings.displayWidth = width;
+        settings.enableDownloads = true;
+        settings.enableBranchVisualizations = false;
+        settings.enableCollapseByBranchLenghts = false;
+        settings.enableCollapseByFeature = true;
+        settings.enableNodeVisualizations = false;
+        settings.nhExportWriteConfidences = true;
+        settings.reCenterAfterCollapse = false;
+        settings.rootOffset = 140;
+
+        //tree = archaeopteryx.parsePhyloXML(data);
+        console.log(x.treestring);
+        tree = archaeopteryx.parseNewHampshire(x.treestring);
+        tre  = tree;
+        archaeopteryx.launch(el, tree, options={}, settings={});
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
