24b4773f51d10d523ce31362e125ec7108f5c487
zach charlop-powers <zach.charlop.powers@gmail.com>
2017-05-05 13:17:50 -0400

    add sequence data test

--- tests/testthat/test-phyloJ.R
+++ tests/testthat/test-phyloJ.R
@@ -23,3 +23,29 @@ test_that("PhyloJ objects can be created correctly", {
 
 })
 
+
+test_that("Adding Sequence data to phyloXML outputs", {
+
+  tree <- ape::read.tree(text="(mammal,(turtle,rayfinfish,(frog,salamander)));")
+  seqs <- c("AAA","CCC","TTT","TTG","GTG")
+  dna  <- Biostrings::DNAStringSet(seqs)
+  names(dna) <- c("mammal", "turtle", "rayfinfish", "frog", "salamander")
+
+  phyJ  <- create_phyloJ(phylo = tree, seqs = dna)
+
+  # sequences are NOT found in treestring1
+  expect_true(
+    all.equal(
+      as.logical(lapply(seqs,  function(s) { grepl(s, write_phyloXML(phyJ))})),
+      c(FALSE, FALSE, FALSE, FALSE, FALSE)))
+
+  # add the sequences to the tree
+  add_sequences(phyJ)
+
+  # sequences ARE found in treestring2
+  expect_true(
+    all.equal(
+      as.logical(lapply(seqs,  function(s) { grepl(s, write_phyloXML(phyJ))})),
+      c(TRUE, TRUE, TRUE, TRUE, TRUE)))
+})
+
